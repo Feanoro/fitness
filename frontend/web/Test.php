05f243bb9a59ec36531fcd81c6194a8520f1d809
Feanoro <ers.cesar@gmail.com>
2016-08-01 18:49:56 -0400

    Initialazing project

---
+++ frontend/web/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hugo
+ * Date: 01/08/2016
+ * Time: 5:56 PM
+ */
+
+namespace frontend\web;
+
+class Test
+{
+    public function __construct()
+    {
+        die("hola");
+    }
+}
---
+++ test/TestCase.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hugo
+ * Date: 01/08/2016
+ * Time: 6:36 PM
+ */
+
+namespace test;
+
+
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+    public function test(){
+
+    }
+}
